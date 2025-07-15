f14c43e4831b8610ffdd44bfa0a692cdae24965e
Misha <misharahnema@gmail.com>
2025-07-15 16:56:55 +0200

    feat: add has_many associations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  # Associations
+  has_many :objectives, dependent: :destroy
+  has_many :tasks, through: :objectives
+
+  # Validations
   validates :email, presence: true, uniqueness: true
 end
