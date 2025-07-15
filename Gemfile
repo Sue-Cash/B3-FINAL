91d21abd71eba80d58408f4444cf1b47e380fe57
Misha <misharahnema@gmail.com>
2025-07-15 16:53:16 +0200

    feat: add Objective model with associations and validations

--- Gemfile
+++ Gemfile
@@ -41,3 +41,4 @@ group :development, :test do
 end
 
 
+gem 'bcrypt', '~> 3.1.18'
---
+++ app/models/objective.rb
@@ -0,0 +1,7 @@
+class Objective < ApplicationRecord
+  belongs_to :user
+  has_many :tasks, dependent: :destroy
+
+  validates :title, presence: true
+  validates :due_date, presence: true
+end
